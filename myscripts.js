52f4c8b7154cf174062e32721fdf50aca24015b3
Gliro7 <glinsek.rok3@gmail.com>
2022-10-10 21:37:33 +0200

    Add game function

--- myscripts.js
+++ myscripts.js
@@ -8,26 +8,51 @@ function getComputerChoice (){
     let computerChoice = computerOptions[randomIndex];
     return computerChoice;
 }
+let playerInput;
+let computerChoice;
 
-let computerChoice = getComputerChoice();
-console.log(computerChoice);
-let playerInput = prompt("rock, paper, scissors?");
-let playerInputAllLow = playerInput.toLowerCase();
-let result = play(playerInputAllLow,computerChoice);
-console.log(result);
-
-function play (playerInp,computerCho){
+function play (playerInp, computerCho){
     if (computerCho === playerInp){
-        return ('Its a tie');
+        return ('Tie');
     }
     else if (
         (computerCho === "rock" && playerInp === "scissors")||
         (computerCho === "scissors" && playerInp === "paper")||
         (computerCho === "paper" && playerInp === "rock")
     ){
-        return (`You lost, ${computerCho} beats ${playerInputAllLow}`);
+        return (`Lose`);
     }
-    else {return (`You won, ${playerInputAllLow} beats ${computerCho}`);}
+    else {return (`Win`);}
 }
+function game(playerInp, computerCho){
+    let tiecounter = 0;
+    let wincounter = 0;
+    let losecounter = 0;
     
-
+    for (let i = 0; i < 5; i++){
+    playerInput = prompt("rock, paper, scissors?").toLowerCase();
+    if (playerInput !== "rock" && playerInput !== "paper" && playerInput !== "scissors"){
+    console.log("enter valid move");
+    break;
+    }
+    computerChoice = getComputerChoice();
+    
+    let result = play(playerInput, computerChoice);
+        
+    if (result === `Tie`){
+        tiecounter += 1;
+        console.log(`Tie, you played ${playerInput} and computer played ${computerChoice}`);
+    }
+    else if (result === `Lose`){
+        losecounter += 1;
+        console.log(`Loss, you played ${playerInput} and computer played ${computerChoice}`);
+    }    
+    else {wincounter += 1;
+        console.log(`Win, you played ${playerInput} and computer played ${computerChoice}`);
+    }      
+    }
+    
+    let finalResult = `You won ${wincounter}, lost ${losecounter} and tied ${tiecounter}.`;
+    return finalResult;
+}
+ console.log(game());
