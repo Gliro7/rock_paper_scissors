611ce7d2cc52e070b3b6fb531693a13697a2d8da
Gliro7 <glinsek.rok3@gmail.com>
2022-10-10 20:41:30 +0200

    Add function play

--- myscripts.js
+++ myscripts.js
@@ -8,9 +8,26 @@ function getComputerChoice (){
     let computerChoice = computerOptions[randomIndex];
     return computerChoice;
 }
-console.log(getComputerChoice());
 
+let computerChoice = getComputerChoice();
+console.log(computerChoice);
+let playerInput = prompt("rock, paper, scissors?");
+let playerInputAllLow = playerInput.toLowerCase();
+let result = play(playerInputAllLow,computerChoice);
+console.log(result);
 
+function play (playerInp,computerCho){
+    if (computerCho === playerInp){
+        return ('Its a tie');
+    }
+    else if (
+        (computerCho === "rock" && playerInp === "scissors")||
+        (computerCho === "scissors" && playerInp === "paper")||
+        (computerCho === "paper" && playerInp === "rock")
+    ){
+        return (`You lost, ${computerCho} beats ${playerInputAllLow}`);
+    }
+    else {return (`You won, ${playerInputAllLow} beats ${computerCho}`);}
+}
     
 
-}
